09eb7688befc8efd275c9870e5caa977ea2d9f0f
GitHub <noreply@github.com>
2024-07-07 02:26:50 +0000

    Bump flake-parts/default/packages/nicksfetch/nvfetcher.toml
    Co-authored-by: sukhmancs <sukhmancs@users.noreply.github.com>

--- flake-parts/default/packages/nicksfetch/generated.nix
+++ flake-parts/default/packages/nicksfetch/generated.nix
@@ -9,7 +9,7 @@
       repo = "neofetch";
       rev = "ccd5d9f52609bbdcd5d8fa78c4fdb0f12954125f";
       fetchSubmodules = false;
-      sha256 = "sha256-9MoX6ykqvd2iB0VrZCfhSyhtztMpBTukeKejfAWYW1w=";
+      sha256 = "sha256-XAg9X6NnfdoxH0A9M2pnN6pSwk9rCC9s3fsOmHo3eJ8=";
     };
     date = "2021-12-10";
   };
