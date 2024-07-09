1da16ff2e043dd9282e3892bcfe9ad2531b2e808
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-09 03:56:16 -0400

    added some tweask

--- machines/triangulum/overrides/system.nix
+++ machines/triangulum/overrides/system.nix
@@ -19,8 +19,7 @@
         efiSupport = lib.mkForce false;
         enableCryptodisk = false;
         theme = null;
-        # This option sets the background color of the system.
-        # If set to null, the background color will be unset.
+        # Background color to be used for GRUB to fill the areas the image isn’t filling
         backgroundColor = null;
         splashImage = null;
         device = lib.mkForce "/dev/vda";
@@ -28,18 +27,8 @@
       };
 
       tmp = {
-        # /tmp on tmpfs, lets it live on your ram
-        # it defaults to FALSE, which means you will use disk space instead of ram
-        # enable tmpfs tmp on anything except servers and builders
         useTmpfs = lib.mkForce false;
-
-        # If not using tmpfs, which is naturally purged on reboot, we must clean
-        # /tmp ourselves. /tmp should be volatile storage!
         cleanOnBoot = lib.mkDefault (!config.boot.tmp.useTmpfs);
-
-        # The size of the tmpfs, in percentage form
-        # this defaults to 50% of your ram, which is a good default
-        # but should be tweaked based on your systems capabilities
         tmpfsSize = lib.mkDefault "75%";
       };
     };
