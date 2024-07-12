f8bf4ce6f386704b5262036a46d22edd4e04f8d0
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-12 01:07:18 -0400

    added some tweask

--- modules/roles/server/services/networking/wireguard/server.nix
+++ modules/roles/server/services/networking/wireguard/server.nix
@@ -35,11 +35,14 @@ in {
 
           # IPs
           ips = [
-            "10.255.255.10/24" # v4 general
-            "10.255.255.254/24" # v4 adtl
-            "fe80::10/64" # v6 link local
-            "2a01:4f9:c010:2cf9:f::10/80" #v6 general
-            "2a01:4f9:c010:2cf9:f::ffff/80" #v6 adtl
+            "10.0.0.1/24"
+            "fdc9:281f:04d7:9ee9::1/64"
+
+            # "10.255.255.10/24" # v4 general
+            # "10.255.255.254/24" # v4 adtl
+            # "fe80::10/64" # v6 link local
+            # "2a01:4f9:c010:2cf9:f::10/80" #v6 general
+            # "2a01:4f9:c010:2cf9:f::ffff/80" #v6 adtl
           ];
 
           # Peers
@@ -57,6 +60,7 @@ in {
             {
               allowedIPs = [
                 "10.0.0.2/24"
+                "fdc9:281f:04d7:9ee9::2/128"
               ];
               publicKey = "T0y8gISdCvbgwH2wOinfX3YuiAfb7zuw20li7dzt1QU=";
             }
@@ -64,6 +68,7 @@ in {
             {
               allowedIPs = [
                 "10.0.0.3/32"
+                "fdc9:281f:04d7:9ee9::3/128"
               ];
               publicKey = "7SUc29cwNgNsvNh95ffsRyQHadDehnO3aC4UHJ8DAXM=";
             }
