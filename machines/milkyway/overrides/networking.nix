b4187fc561635b323ff523d0b8c63813c739bf83
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-16 16:39:38 -0400

    added chatgpt waifu booru chatgpt chat apps to ags bar

--- machines/milkyway/overrides/networking.nix
+++ machines/milkyway/overrides/networking.nix
@@ -7,8 +7,8 @@ in {
   config = {
     services.resolved.enable = mkForce false; # use adguardhome for dns
     networking = {
-      useDHCP = false;
-      useNetworkd = false;
+      useDHCP = mkForce false;
+      useNetworkd = mkForce false;
       networkmanager.dns = mkForce "default";
       nameservers = mkForce ["102.209.85.226"]; # adguardhome dns server
     };
