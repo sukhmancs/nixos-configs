433eb873274182b17070e5dc01427bc8c1a9f909
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-16 12:55:43 -0400

    added chatgpt waifu booru chatgpt chat apps to ags bar

--- modules/exclusive/home-manager/bars/ags/sideleft/main.js
+++ modules/exclusive/home-manager/bars/ags/sideleft/main.js
@@ -1,4 +1,5 @@
 import PopupWindow from "./.widgethacks/popupwindow.js";
+import userOptions from './user_options.js';
 import SidebarLeft from "./sideleft.js";
 import Widget from "resource:///com/github/Aylur/ags/widget.js";
 const { Box } = Widget;
