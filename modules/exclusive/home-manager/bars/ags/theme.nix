7925cf0df847ac7c641c8ff994273067402fa677
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-19 13:13:34 -0400

    ags bar fix

--- modules/exclusive/home-manager/bars/ags/theme.nix
+++ modules/exclusive/home-manager/bars/ags/theme.nix
@@ -21,20 +21,20 @@
     $author: "${colors.author}";
     $base00: #${colors.base00}; // base
     $base01: #${colors.base01}; // mantle
-    $base02: "${colors.base02}"; // surface0
-    $base03: "${colors.base03}"; // surface1
-    $base04: "${colors.base04}"; // surface2
-    $base05: "${colors.base05}"; // text
-    $base06: "${colors.base06}"; // rosewater
-    $base07: "${colors.base07}"; // lavender
-    $base08: "${colors.base08}"; // red
-    $base09: "${colors.base09}"; // peach
-    $base0A: "${colors.base0A}"; // yellow
-    $base0B: "${colors.base0B}"; // green
-    $base0C: "${colors.base0C}"; // teal
-    $base0D: "${colors.base0D}"; // blue
-    $base0E: "${colors.base0E}"; // mauve
-    $base0F: "${colors.base0F}"; // flamingo
+    $base02: #${colors.base02}; // surface0
+    $base03: #${colors.base03}; // surface1
+    $base04: #${colors.base04}; // surface2
+    $base05: #${colors.base05}; // text
+    $base06: #${colors.base06}; // rosewater
+    $base07: #${colors.base07}; // lavender
+    $base08: #${colors.base08}; // red
+    $base09: #${colors.base09}; // peach
+    $base0A: #${colors.base0A}; // yellow
+    $base0B: #${colors.base0B}; // green
+    $base0C: #${colors.base0C}; // teal
+    $base0D: #${colors.base0D}; // blue
+    $base0E: #${colors.base0E}; // mauve
+    $base0F: #${colors.base0F}; // flamingo
   '';
 
   variablesFile = builtins.toFile "variables.scss" scssContent;
