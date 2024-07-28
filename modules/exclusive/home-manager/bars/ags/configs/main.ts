bda201ca76a5748090d1dcc400171789407af2e4
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-28 15:21:31 -0400

    update ags

--- modules/exclusive/home-manager/bars/ags/configs/main.ts
+++ modules/exclusive/home-manager/bars/ags/configs/main.ts
@@ -1,47 +1,47 @@
-import "lib/session"
-import "lib/init"
-import "style/style"
-import options from "options"
-import Bar from "widget/bar/Bar"
-import Launcher from "widget/launcher/Launcher"
-import NotificationPopups from "widget/notifications/NotificationPopups"
-import OSD from "widget/osd/OSD"
-import Overview from "widget/overview/Overview"
-import PowerMenu from "widget/powermenu/PowerMenu"
-import ScreenCorners from "widget/bar/ScreenCorners"
-import SettingsDialog from "widget/settings/SettingsDialog"
-import Verification from "widget/powermenu/Verification"
-import { forMonitors } from "lib/utils"
+import "lib/session";
+import "lib/init";
+import "style/style";
+import options from "options";
+import Bar from "widget/bar/Bar";
+import Launcher from "widget/launcher/Launcher";
+import NotificationPopups from "widget/notifications/NotificationPopups";
+import OSD from "widget/osd/OSD";
+import Overview from "widget/overview/Overview";
+import PowerMenu from "widget/powermenu/PowerMenu";
+import ScreenCorners from "widget/bar/ScreenCorners";
+import SettingsDialog from "widget/settings/SettingsDialog";
+import Verification from "widget/powermenu/Verification";
+import { forMonitors } from "lib/utils";
 import {
-  CornerTopleft,
-  CornerTopright,
-  CornerBottomright,
-  CornerBottomleft
+    CornerTopleft,
+    CornerTopright,
+    CornerBottomright,
+    CornerBottomleft,
 } from "./roundedCorners/index.js";
-import userOptions from './sideleft/.configuration/user_options.js';
-import { setupQuickSettings } from "widget/quicksettings/QuickSettings"
-import { setupCalendarSettings } from "widget/calendarsettings/CalendarSettings"
-import SideLeft from "sideleft/main.js"
-import hyprland from "lib/hyprland"
+import userOptions from "./sideleft/.configuration/user_options.js";
+import { setupQuickSettings } from "widget/quicksettings/QuickSettings";
+import { setupCalendarSettings } from "widget/calendarsettings/CalendarSettings";
+import SideLeft from "sideleft/main.js";
+import hyprland from "lib/hyprland";
 
 App.config({
     onConfigParsed: () => {
-        setupQuickSettings()
-        setupCalendarSettings()
-        hyprland()
+        setupQuickSettings();
+        setupCalendarSettings();
+        hyprland();
     },
     closeWindowDelay: {
-        "launcher": options.transition.value,
-        "overview": options.transition.value,
-        "quicksettings": options.transition.value,
-        "calendarsettings": options.transition.value,
+        launcher: options.transition.value,
+        overview: options.transition.value,
+        quicksettings: options.transition.value,
+        calendarsettings: options.transition.value,
     },
     windows: () => [
         ...forMonitors(Bar),
         ...forMonitors(NotificationPopups),
         // ...forMonitors(ScreenCorners),
         ...forMonitors(OSD),
-        SideLeft(),
+        // SideLeft(), // TODO:: core dump
         Launcher(),
         Overview(),
         PowerMenu(),
@@ -52,4 +52,4 @@ App.config({
         // CornerBottomright(),
         // CornerBottomleft(),
     ],
-})
+});
--- modules/exclusive/home-manager/bars/ags/configs/widget/bar/Bar.ts
+++ modules/exclusive/home-manager/bars/ags/configs/widget/bar/Bar.ts
@@ -1,23 +1,23 @@
-import Cava from "./buttons/Cava"
-import Date from "./buttons/Date"
+import Cava from "./buttons/Cava";
+import Date from "./buttons/Date";
 // import Launcher from "./buttons/Launcher"
-import Launcher from "./buttons/Launcher"
-import Chat from "./buttons/Chat"
-import Media from "./buttons/Media"
-import PowerMenu from "./buttons/PowerMenu"
-import SysTray from "./buttons/SysTray"
-import SystemIndicators from "./buttons/SystemIndicators"
-import Taskbar from "./buttons/Taskbar"
-import Workspaces from "./buttons/Workspaces"
-import {RoundedAngleEnd} from "../../roundedCorners/index.js";
-import Wallpapers from "./buttons/Wallpapers"
-import BatteryBar from "./buttons/BatteryBar"
-import options from "options"
+import Launcher from "./buttons/Launcher";
+import Chat from "./buttons/Chat";
+import Media from "./buttons/Media";
+import PowerMenu from "./buttons/PowerMenu";
+import SysTray from "./buttons/SysTray";
+import SystemIndicators from "./buttons/SystemIndicators";
+import Taskbar from "./buttons/Taskbar";
+import Workspaces from "./buttons/Workspaces";
+import { RoundedAngleEnd } from "../../roundedCorners/index.js";
+import Wallpapers from "./buttons/Wallpapers";
+import BatteryBar from "./buttons/BatteryBar";
+import options from "options";
 
-const { start, center, end } = options.bar.layout
-const pos = options.bar.position.bind()
+const { start, center, end } = options.bar.layout;
+const pos = options.bar.position.bind();
 
-export type BarWidget = keyof typeof widget
+export type BarWidget = keyof typeof widget;
 
 const widget = {
     battery: BatteryBar,
@@ -32,59 +32,58 @@ const widget = {
     workspaces: Workspaces,
     wallpapers: Wallpapers,
     expander: () => Widget.Box({ expand: true }),
-}
+};
 
-export default (monitor: number) => Widget.Window({
-    monitor,
-    class_name: "bar",
-    name: `bar${monitor}`,
-    exclusivity: "exclusive",
-    anchor: ['top', 'left', 'right'],
-    layer: "top",
-    margins: [8, 8, 0, 8],
-    child: Widget.CenterBox({
-        css: "min-height: 1px; min-width: 1px;",
-        startWidget: Widget.Box({
-            hexpand: true,
-            children: [
-                Widget.Box({
-                    className: "barLeft",
-                    children: [
-                        Launcher(),
-                        Chat(),
-                        Media(monitor, "start"),
-                        // RoundedAngleEnd("topright", {class_name: "angle"})
-                    ]
-                }),
-            ]
-        }),
-        centerWidget: Widget.Box({
-            hpack: "center",
-            children: [
-                Widget.Box({
-                    className: "barCenter",
-                    children: [
-                        Workspaces(),
-                    ]
-                }),
-            ]
-        }),
-        endWidget: Widget.Box({
-            hexpand: true,
-            children: [
-                Widget.Box({ expand: true }),
-                Widget.Box({
-                    className: "barRight",
-                    children: [
-                        // RoundedAngleEnd("topleft", {class_name: "angle", click_through: true}),
-                        // Cava(monitor, "end"),
-                        SysTray(monitor, "end"),
-                        BatteryBar(monitor, "end"),
-                        SystemIndicators(monitor, "end"),
-                        Date(monitor, "last"),
-                        ]
+export default (monitor: number) =>
+    Widget.Window({
+        monitor,
+        class_name: "bar",
+        name: `bar${monitor}`,
+        exclusivity: "exclusive",
+        anchor: ["top", "left", "right"],
+        layer: "top",
+        margins: [8, 8, 0, 8],
+        child: Widget.CenterBox({
+            css: "min-height: 1px; min-width: 1px;",
+            startWidget: Widget.Box({
+                hexpand: true,
+                children: [
+                    Widget.Box({
+                        className: "barLeft",
+                        children: [
+                            Launcher(),
+                            // Chat(),
+                            Media(monitor, "start"),
+                            // RoundedAngleEnd("topright", {class_name: "angle"})
+                        ],
                     }),
-                ]
+                ],
+            }),
+            centerWidget: Widget.Box({
+                hpack: "center",
+                children: [
+                    Widget.Box({
+                        className: "barCenter",
+                        children: [Workspaces()],
+                    }),
+                ],
+            }),
+            endWidget: Widget.Box({
+                hexpand: true,
+                children: [
+                    Widget.Box({ expand: true }),
+                    Widget.Box({
+                        className: "barRight",
+                        children: [
+                            // RoundedAngleEnd("topleft", {class_name: "angle", click_through: true}),
+                            // Cava(monitor, "end"),
+                            SysTray(monitor, "end"),
+                            BatteryBar(monitor, "end"),
+                            SystemIndicators(monitor, "end"),
+                            Date(monitor, "last"),
+                        ],
+                    }),
+                ],
+            }),
         }),
-    }),
-})
+    });
