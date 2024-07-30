bfbda8cdde76d3b611de36b2e2f1290d83c2c95d
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-07-30 04:34:17 -0400

    use tofi temporarly for iso

--- machines/messier/homes/waybar/presets/style.nix
+++ machines/messier/homes/waybar/presets/style.nix
@@ -14,8 +14,7 @@ in ''
   window#waybar {
     background-color: #${base00};
     border: .5px solid #${base01};
-    border-radius: 20px;
-    box-shadow: 2 3 2 2px #151515;
+    border-radius: 0px;
     color: #${base05};
     margin: 8px 8px;
     transition-property: background-color;
