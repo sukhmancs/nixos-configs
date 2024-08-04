28a1ab24eab5f77ea6ee9ed3c05b9dde1c1e50a1
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 03:34:58 -0400

    README.md: update todo

--- homes/shared/programs/newsboat/scripts/rofiHandler.nix
+++ homes/shared/programs/newsboat/scripts/rofiHandler.nix
@@ -16,7 +16,7 @@ in
 
     feed="$1"
 
-    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\\n<span font='FontAwesome'>\uf03e </span> imv\\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float" | rofi -dmenu -i -p "Open it with?" -markup-rows)" in
+    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //')" in
         browser) setsid -f firefox "$feed" ;; #>/dev/null 2>&1 ;;
         nimv) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.imv} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
         PDF) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.zathura} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
