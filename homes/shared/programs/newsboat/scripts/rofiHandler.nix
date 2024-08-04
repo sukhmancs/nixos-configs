983bc4736fc5a4b744546d78c779d78c36963bc2
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 03:45:20 -0400

    h/s/p/newsboat: add rofihandler script to manage newsboat

--- homes/shared/programs/newsboat/scripts/rofiHandler.nix
+++ homes/shared/programs/newsboat/scripts/rofiHandler.nix
@@ -16,12 +16,12 @@ in
 
     feed="$1"
 
-    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //')" in
-        browser) setsid -f firefox "$feed" ;; #>/dev/null 2>&1 ;;
+    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //; s/%$//')" in
+        browser) setsid -f firefox "$feed" >/dev/null 2>&1 ;;
         nimv) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.imv} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
         PDF) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.zathura} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
-        lynx) ${getExe pkgs.lynx} "$feed" ;; # >/dev/null 2>&1 ;;
-        vim) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && setsid -f "foot" -e "$EDITOR" "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" ;; #  >/dev/null 2>&1 ;;
+        lynx) ${getExe pkgs.lynx} "$feed" >/dev/null 2>&1 ;;
+        vim) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && setsid -f "foot" -e "$EDITOR" "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" >/dev/null 2>&1 ;;
         mpv) setsid -f mpv -quiet "$feed" >/dev/null 2>&1 ;;
         "mpv loop") setsid -f mpv -quiet --loop "$feed" >/dev/null 2>&1 ;;
         "mpv float") setsid -f "$TERMINAL" -e mpv --geometry=+0-0 --autofit=30%  --title="mpvfloat" "$feed" >/dev/null 2>&1 ;;
