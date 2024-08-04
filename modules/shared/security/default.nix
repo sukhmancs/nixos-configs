0888e0a580e9a1ec786b2ee1419f162a512865df
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 19:47:05 -0400

    m/s/security: fix security bugs

--- modules/shared/security/default.nix
+++ modules/shared/security/default.nix
@@ -3,13 +3,13 @@
     ./apparmor.nix # apparmor configuration and policies
     ./auditd.nix # auditd
     ./clamav.nix # clamav antivirus
-    # ./impurity-tracker.nix # find applications that depend on /bin/sh or /usr/bin/env
+    ./impurity-tracker.nix # find applications that depend on /bin/sh or /usr/bin/env
     ./kernel.nix # kernel hardening
     # ./memalloc.nix # memory allocator hardening
     ./pam.nix # pam configuration
     ./pki.nix # pki certificate bundles
     ./polkit.nix # polkit configuration
-    # ./selinux.nix # selinux support + kernel patches
+    ./selinux.nix # selinux support + kernel patches
     ./sudo.nix # sudo rules and configuration
     ./virtualization.nix # hypervisor hardening
     ./usbguard.nix # usbguard
