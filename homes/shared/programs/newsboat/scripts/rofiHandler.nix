b967228c5ee2c77b77d70a8c2b096472b4b09cbe
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 05:31:19 -0400

    h/s/p/newsboat: add some tweaks

--- homes/shared/programs/newsboat/scripts/rofiHandler.nix
+++ homes/shared/programs/newsboat/scripts/rofiHandler.nix
@@ -30,9 +30,9 @@ in
 
     case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float\n" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //; s/%$//')" in
         browser) setsid -f $BROWSER "$feed" >/dev/null 2>&1 ;;
-        nimv) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.imv}/bin/imv "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
-        PDF) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.zathura}/bin/zathura "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
-        lynx) ${builtins.toString pkgs.lynx}/bin/lynx -accept_all_cookies "$feed" >/dev/null 2>&1 ;;
+        nimv) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${getExe pkgs.imv} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
+        PDF) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${getExe pkgs.zathura} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
+        lynx) ${getExe pkgs.lynx} -accept_all_cookies "$feed" >/dev/null 2>&1 ;;
         vim) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && setsid -f "$TERMINAL" -e "$EDITOR" "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" >/dev/null 2>&1 ;;
         mpv) setsid -f mpv -quiet "$feed" >/dev/null 2>&1 ;;
         "mpv loop") setsid -f mpv -quiet --loop "$feed" >/dev/null 2>&1 ;;
