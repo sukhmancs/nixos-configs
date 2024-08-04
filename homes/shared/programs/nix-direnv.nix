0de2287f7060fbc8e334155dfda72dc2f8bbea45
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-03 22:39:36 -0400

    h/s/p/nix-direnv.nix: default settings for direnv

--- homes/shared/programs/nix-direnv.nix
+++ homes/shared/programs/nix-direnv.nix
@@ -1,10 +1,20 @@
+#
+# Direnv - load/unload environment variables based on the current directory
+#
+# Direnv is an environment switcher for the shell. It knows how to hook into
+# bash, zsh, tcsh, fish shell and elvish to load or unload environment variables
+# depending on the current directory. This allows project-specific environment
+# variables.
 {pkgs, ...}: {
+  # Set the log format to an empty string to avoid direnv from polluting the
+  # shell output with log messages.
   home.sessionVariables = {
     DIRENV_LOG_FORMAT = "";
   };
 
   programs.direnv = {
     enable = true;
+    # nix-direnv enables the use of use_nix and use_flake in .envrc files.
     nix-direnv.enable = true;
     # stdlib = ''
     #   : ''${XDG_CACHE_HOME:=$HOME/.cache}
