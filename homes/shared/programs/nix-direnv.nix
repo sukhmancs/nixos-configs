53cd9cb8897b5b76a807946278b6b3d0d966a3d9
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-03 22:34:53 -0400

    h/s/p/nix-direnv.nix: default settings for direnv

--- homes/shared/programs/nix-direnv.nix
+++ homes/shared/programs/nix-direnv.nix
@@ -6,16 +6,16 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
-    stdlib = ''
-      : ''${XDG_CACHE_HOME:=$HOME/.cache}
-      declare -A direnv_layout_dirs
-      direnv_layout_dir() {
-          echo "''${direnv_layout_dirs[$PWD]:=$(
-              echo -n "$XDG_CACHE_HOME"/direnv/layouts/
-              echo -n "$PWD" | ${pkgs.perl}/bin/shasum| cut -d ' ' -f 1
-          )}"
-      }
-    '';
+    # stdlib = ''
+    #   : ''${XDG_CACHE_HOME:=$HOME/.cache}
+    #   declare -A direnv_layout_dirs
+    #   direnv_layout_dir() {
+    #       echo "''${direnv_layout_dirs[$PWD]:=$(
+    #           echo -n "$XDG_CACHE_HOME"/direnv/layouts/
+    #           echo -n "$PWD" | ${pkgs.perl}/bin/shasum| cut -d ' ' -f 1
+    #       )}"
+    #   }
+    # '';
 
     # we should probably do this ourselves
     enableZshIntegration = true;
--- homes/shared/programs/nix-index.nix
+++ homes/shared/programs/nix-index.nix
@@ -1,5 +1,22 @@
+###
+### Nix-index provides a "command-not-found" script.
+###
+### Run following command to download the database
+### without any database command-not-found will not work:
+###
+###   $ nix run github:nix-community/nix-index#nix-index
+###
+### Example output:
+### $ blender
+###  The program 'blender' is currently not installed. You can install it
+###  by typing:
+###    nix-env -iA nixpkgs.blender.out
+###
+###  Or run it once with:
+###    nix-shell -p blender.out --run ...
+###
 {inputs, ...}: {
-  imports = [inputs.nix-index-db.hmModules.nix-index];
+  # imports = [inputs.nix-index-db.hmModules.nix-index];
 
   config = {
     home.sessionVariables = {
@@ -8,16 +25,16 @@
     };
 
     programs = {
-      nix-index-database.comma.enable = true;
+      # nix-index-database.comma.enable = true;
 
       # `command-not-found` relies on nix-channel.
       # Enable and use `nix-index` instead.
       command-not-found.enable = false;
       nix-index = {
         enable = true;
-
+        enableZshIntegration = true;
         # link nix-inde database to ~/.cache/nix-index
-        symlinkToCacheHome = true;
+        # symlinkToCacheHome = true;
       };
     };
   };
