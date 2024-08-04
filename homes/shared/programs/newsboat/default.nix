14b520108be485dc95ac57f89a878252fee45434
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 04:39:59 -0400

    h/s/p/newsboat: add some tweaks

--- homes/shared/programs/newsboat/default.nix
+++ homes/shared/programs/newsboat/default.nix
@@ -15,11 +15,9 @@
   lynx = "${getExe pkgs.lynx}";
   yt = "${getExe pkgs.yt-dlp}";
   wlc = "${getExe pkgs.wl-clipboard}";
-  #   rofiHandler = import ./scripts/menuHandler.nix {inherit pkgs;};
 in {
   config = {
     home.packages = with pkgs; [
-      #   lynx
       (import ./scripts/rofiHandler.nix {inherit pkgs;})
     ];
 
@@ -30,59 +28,59 @@ in {
       inherit ((import ./urls.nix)) urls;
 
       extraConfig = ''
-                # error-log /dev/null
-                # download-full-page yes
-                # download-retries 3
-                # cookie-cache ~/.cache/newsboat/cookies.txt
-                # auto-reload yes
-                # max-items 0
-                # scrolloff 999
-                # reload-threads 100
-                # bind-key j down
-                # bind-key k up
-                # bind-key j next articlelist
-                # bind-key k prev articlelist
-                # bind-key J next-feed articlelist
-                # bind-key K prev-feed articlelist
-                # bind-key G end
-                # bind-key g home
-                # bind-key d pagedown
-                # bind-key u pageup
-                # bind-key l open
-                # bind-key h quit
-                # bind-key a toggle-article-read
-                # bind-key n next-unread
-                # bind-key N prev-unread
-                # bind-key D pb-download
-                # bind-key U show-urls
-                # bind-key x pb-delete
-                # color listnormal         color15 default
-                # color listnormal_unread  color2  default
-                # color listfocus_unread   color2  color0
-                # color listfocus          default color0
-                # color background         default default
-                # color article            default default
-                # color end-of-text-marker color8  default
-                # color info               color4  color8
-                # color hint-separator     default color8
-                # color hint-description   default color8
-                # color title              color14 color8
-                # highlight article "^(Feed|Title|Author|Link|Date): .+" color4 default bold
-                # highlight article "^(Feed|Title|Author|Link|Date):" color14 default bold
-                # highlight article "\\((link|image|video)\\)" color8 default
-                # highlight article "https?://[^ ]+" color4 default
-                # highlight article "\[[0-9]+\]" color6 default bold
-                # user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-                # html-renderer "${pandoc} --from=html -t markdown_github-raw_html"
-                # pager "${glow} --pager --width 72"
-                # # macros
-                # macro v set browser "${mpv} %u" ; open-in-browser ; set browser "firefox %u" -- "Open video on mpv"
-                # macro , open-in-browser
+        # error-log /dev/null
+        # download-full-page yes
+        # download-retries 3
+        # cookie-cache ~/.cache/newsboat/cookies.txt
+        # auto-reload yes
+        # max-items 0
+        # scrolloff 999
+        # reload-threads 100
+        # bind-key j down
+        # bind-key k up
+        # bind-key j next articlelist
+        # bind-key k prev articlelist
+        # bind-key J next-feed articlelist
+        # bind-key K prev-feed articlelist
+        # bind-key G end
+        # bind-key g home
+        # bind-key d pagedown
+        # bind-key u pageup
+        # bind-key l open
+        # bind-key h quit
+        # bind-key a toggle-article-read
+        # bind-key n next-unread
+        # bind-key N prev-unread
+        # bind-key D pb-download
+        # bind-key U show-urls
+        # bind-key x pb-delete
+        # color listnormal         color15 default
+        # color listnormal_unread  color2  default
+        # color listfocus_unread   color2  color0
+        # color listfocus          default color0
+        # color background         default default
+        # color article            default default
+        # color end-of-text-marker color8  default
+        # color info               color4  color8
+        # color hint-separator     default color8
+        # color hint-description   default color8
+        # color title              color14 color8
+        # highlight article "^(Feed|Title|Author|Link|Date): .+" color4 default bold
+        # highlight article "^(Feed|Title|Author|Link|Date):" color14 default bold
+        # highlight article "\\((link|image|video)\\)" color8 default
+        # highlight article "https?://[^ ]+" color4 default
+        # highlight article "\[[0-9]+\]" color6 default bold
+        # user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        # html-renderer "${pandoc} --from=html -t markdown_github-raw_html"
+        # pager "${glow} --pager --width 72"
+        # # macros
+        # macro v set browser "${mpv} %u" ; open-in-browser ; set browser "firefox %u" -- "Open video on mpv"
+        # macro , open-in-browser
 
 
 
 
-                #show-read-feeds no
+        #show-read-feeds no
         auto-reload yes
 
         external-url-viewer "${urs} -dc -r 'firefox {}'"
@@ -115,18 +113,14 @@ in {
 
         browser "firefox %u"
         macro , open-in-browser
-        # user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
         html-renderer "${pandoc} --from=html -t markdown_github-raw_html"
         pager "${glow} --pager --width 72"
         macro r set browser "rofiHandler %u" ; open-in-browser ; set browser "firefox %u"
         macro t set browser "firefox %u" ; open-in-browser ; set browser "firefox %u"
         macro v set browser "setsid -f mpv" ; open-in-browser ; set browser "firefox %u"
-        macro c set browser "echo %u | wl-copy -r -sel c" ; open-in-browser ; set browser "firefox %u"
+        macro c set browser "echo %u | wl-copy && wl-copy --clear" ; open-in-browser ; set browser "firefox %u"
         macro a set browser "${tsp} ${yt} --embed-metadata -xic -f bestaudio/best --restrict-filenames" ; open-in-browser ; set browser "firefox %u"
-        macro w set browser "${lynx}" ; open-in-browser ; set browser "firefox %u"
-        # macro C set browser "youtube-viewer --comments=%u" ; open-in-browser ; set browser "firefox %u"
-        # macro p set browser "peertubetorrent %u 480" ; open-in-browser ; set browser "firefox %u"
-        # macro P set browser "peertubetorrent %u 1080" ; open-in-browser ; set browser "firefox %u"
+        macro w set browser "${lynx}" -anonymous -accept_all_cookies ; open-in-browser ; set browser "firefox %u"
 
         highlight all "---.*---" yellow
         highlight feedlist ".*(0/0))" black
--- homes/shared/programs/newsboat/scripts/rofiHandler.nix
+++ homes/shared/programs/newsboat/scripts/rofiHandler.nix
@@ -12,6 +12,10 @@ in
     # Check if required environment variables are set
     if [ -z "$BROWSER" ] || [ -z "$TERMINAL" ] || [ -z "$EDITOR" ]; then
       echo "Error: Required environment variables BROWSER, TERMINAL, or EDITOR are not set."
+
+      if command -v notify-send >/dev/null 2>&1; then
+        notify-send "Error: Required environment variables BROWSER, TERMINAL, or EDITOR are not set."
+      fi
       exit 1
     fi
 
@@ -22,11 +26,11 @@ in
 
     feed="$1"
 
-    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float\n" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //')" in
+    case "$(printf "<span font='FontAwesome'>\uf268 </span> browser\n<span font='FontAwesome'>\uf03e </span> imv\n<span font='FontAwesome'>\uf1c1 </span> PDF\n<span font='FontAwesome'>\uf0c1 </span> lynx\n<span font='FontAwesome'>\uf044 </span> vim\n<span font='FontAwesome'>\uf144 </span> mpv\n<span font='FontAwesome'>\uf144 </span> mpv loop\n<span font='FontAwesome'>\uf144 </span> mpv float\n" | rofi -dmenu -i -p "Open it with?" -markup-rows | sed 's/.*span> //; s/%$//')" in
         browser) setsid -f $BROWSER "$feed" >/dev/null 2>&1 ;;
         nimv) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.imv} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
         PDF) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && ${pkgs.zathura} "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")"  >/dev/null 2>&1 ;;
-        lynx) ${getExe pkgs.lynx} "$feed" >/dev/null 2>&1 ;;
+        lynx) ${pkgs.lynx} -anonymous -accept_all_cookies "$feed" >/dev/null 2>&1 ;;
         vim) curl -sL "$feed" > "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" && setsid -f "$TERMINAL" -e "$EDITOR" "/tmp/$(echo "$feed" | sed "s|.*/||;s/%20/ /g")" >/dev/null 2>&1 ;;
         mpv) setsid -f mpv -quiet "$feed" >/dev/null 2>&1 ;;
         "mpv loop") setsid -f mpv -quiet --loop "$feed" >/dev/null 2>&1 ;;
