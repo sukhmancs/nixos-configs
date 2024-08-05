c6e5b3afd202d6f4e27b218fb6dab504b10b596b
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-05 14:06:55 -0400

    m/e/h/theme/qt: setup qt theme with kvantum

--- modules/exclusive/home-manager/programs/vscode.nix
+++ modules/exclusive/home-manager/programs/vscode.nix
@@ -1,16 +1,15 @@
 #
 # VSCode - Visual Studio Code
 #
-{ lib
-, pkgs
-, osConfig
-, config
-, ...
-}:
-let
-  inherit (lib) mkIf;
-in
 {
+  lib,
+  pkgs,
+  osConfig,
+  config,
+  ...
+}: let
+  inherit (lib) mkIf;
+in {
   config = mkIf true {
     programs.vscode = {
       enable = true;
@@ -35,30 +34,30 @@ in
           ms-vscode-remote.remote-ssh
           ms-vscode.cpptools
 
-          naumovs.color-highlight
-          #          ms-python.black-formatter
-          svelte.svelte-vscode
-          ms-vsliveshare.vsliveshare
-          oderwat.indent-rainbow
-          pkief.material-icon-theme
-          rust-lang.rust-analyzer
-          shardulm94.trailing-spaces
-          sumneko.lua
-          timonwong.shellcheck
-          usernamehw.errorlens
-          xaver.clang-format
-          yzhang.markdown-all-in-one
-          james-yu.latex-workshop
-          redhat.vscode-yaml
-          ms-azuretools.vscode-docker
-          irongeek.vscode-env
-          github.vscode-pull-request-github
-          github.codespaces
-          astro-build.astro-vscode
-          wakatime.vscode-wakatime
+          #          naumovs.color-highlight
+          #          #          ms-python.black-formatter
+          #          svelte.svelte-vscode
+          #          ms-vsliveshare.vsliveshare
+          #          oderwat.indent-rainbow
+          #          pkief.material-icon-theme
+          #          rust-lang.rust-analyzer
+          #          shardulm94.trailing-spaces
+          #          sumneko.lua
+          #          timonwong.shellcheck
+          #          usernamehw.errorlens
+          #          xaver.clang-format
+          #          yzhang.markdown-all-in-one
+          #          james-yu.latex-workshop
+          #          redhat.vscode-yaml
+          #          ms-azuretools.vscode-docker
+          #          irongeek.vscode-env
+          #          github.vscode-pull-request-github
+          #          github.codespaces
+          #          astro-build.astro-vscode
+          #          wakatime.vscode-wakatime
         ]
         ++ [
-          pkgs.vscode-extensions."2gua".rainbow-brackets
+          #pkgs.vscode-extensions."2gua".rainbow-brackets
         ]
         ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
           {
@@ -73,30 +72,30 @@ in
           #            version = "1.0.12";
           #            sha256 = "sha256-D9E3KRUOlNVXH4oMv1W0+/mbqO8Se7+6E2F5P/KvCro=";
           #          }
-          {
-            name = "vscode-typescript-vue-plugin";
-            publisher = "vue";
-            version = "1.0.12";
-            sha256 = "sha256-WiL+gc9+U861ubLlY/acR+ZcrFT7TdIDR0K1XNNidX8=";
-          }
-          {
-            name = "decay";
-            publisher = "decaycs";
-            version = "1.0.6";
-            sha256 = "sha256-Jtxj6LmHgF7UNaXtXxHkq881BbuPtIJGxR7kdhKr0Uo=";
-          }
-          {
-            name = "vscode-typescript-next";
-            publisher = "ms-vscode";
-            version = "5.0.202301100";
-            sha256 = "sha256-8d/L9F06ZaS9dTOXV6Q40ivI499nfZLQURcLdHXoTSM=";
-          }
-          {
-            name = "vscode-chromium-vector-icons";
-            publisher = "adolfdaniel";
-            version = "1.0.2";
-            sha256 = "sha256-Meo53e/3jUP6YDEXOA/40xghI77jj4iAQus3/S8RPZI=";
-          }
+          #          {
+          #            name = "vscode-typescript-vue-plugin";
+          #            publisher = "vue";
+          #            version = "1.0.12";
+          #            sha256 = "sha256-WiL+gc9+U861ubLlY/acR+ZcrFT7TdIDR0K1XNNidX8=";
+          #          }
+          #          {
+          #            name = "decay";
+          #            publisher = "decaycs";
+          #            version = "1.0.6";
+          #            sha256 = "sha256-Jtxj6LmHgF7UNaXtXxHkq881BbuPtIJGxR7kdhKr0Uo=";
+          #          }
+          #          {
+          #            name = "vscode-typescript-next";
+          #            publisher = "ms-vscode";
+          #            version = "5.0.202301100";
+          #            sha256 = "sha256-8d/L9F06ZaS9dTOXV6Q40ivI499nfZLQURcLdHXoTSM=";
+          #          }
+          #          {
+          #            name = "vscode-chromium-vector-icons";
+          #            publisher = "adolfdaniel";
+          #            version = "1.0.2";
+          #            sha256 = "sha256-Meo53e/3jUP6YDEXOA/40xghI77jj4iAQus3/S8RPZI=";
+          #          }
         ];
       userSettings = {
         #        "workbench.iconTheme" = "material-icon-theme";
@@ -204,11 +203,11 @@ in
         #     "backgroundColor" = "theme.editor.selectionBackground";
         #   };
         # };
-        "breadcrumbs.enabled" = true;
-        "github.copilot.enable" = {
-          "markdown" = true;
-          "plaintext" = true;
-        };
+        #        "breadcrumbs.enabled" = true;
+        #        "github.copilot.enable" = {
+        #          "markdown" = true;
+        #          "plaintext" = true;
+        #        };
       };
     };
   };
