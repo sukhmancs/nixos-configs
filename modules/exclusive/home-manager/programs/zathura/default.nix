bee2add35adc13218d3fdde36c8da6c489a80ba3
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-04 20:10:37 -0400

    m/e/p/zathura: add custom colors directly inside the options

--- modules/exclusive/home-manager/programs/zathura/default.nix
+++ modules/exclusive/home-manager/programs/zathura/default.nix
@@ -6,19 +6,21 @@
   ...
 }: let
   inherit (lib) mkIf;
+  inherit (osConfig) modules;
+  inherit (modules.themes) colors;
 in {
   config = mkIf config.programs.zathura.enable {
-    xdg.configFile."zathura/catppuccin-mocha".source = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
-      hash = "sha256-POxMpm77Pd0qywy/jYzZBXF/uAKHSQ0hwtXD4wl8S2Q=";
-    };
+    # xdg.configFile."zathura/catppuccin-mocha".source = pkgs.fetchurl {
+    #   url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
+    #   hash = "sha256-POxMpm77Pd0qywy/jYzZBXF/uAKHSQ0hwtXD4wl8S2Q=";
+    # };
     #TODO: Hash colors scheme is not being detected
     # xdg.configFile."zathura/custom-theme" = {
     #   text = import ./custom-theme.nix {inherit osConfig;};
     # };
 
     programs.zathura = {
-      extraConfig = "include catppuccin-mocha";
+      # extraConfig = "include custom-theme";
 
       options = {
         font = "Iosevka 15";
@@ -34,6 +36,45 @@ in {
         statusbar-h-padding = "0";
         statusbar-v-padding = "0";
         page-padding = "1";
+
+        default-fg = "#${colors.base05}";
+        default-bg = "#${colors.base02}";
+
+        completion-bg = "#${colors.base00}";
+        completion-fg = "#${colors.base05}";
+        completion-highlight-bg = "#${colors.base03}";
+        completion-highlight-fg = "#${colors.base05}";
+        completion-group-bg = "#${colors.base00}";
+        completion-group-fg = "#${colors.base07}";
+
+        statusbar-fg = "#${colors.base05}";
+        statusbar-bg = "#${colors.base02}";
+
+        notification-bg = "#${colors.base02}";
+        notification-fg = "#${colors.base05}";
+        notification-error-bg = "#${colors.base02}";
+        notification-error-fg = "#${colors.base08}";
+        notification-warning-bg = "#${colors.base02}";
+        notification-warning-fg = "#${colors.base0A}";
+
+        inputbar-fg = "#${colors.base05}";
+        inputbar-bg = "#${colors.base02}";
+
+        recolor = "true";
+        recolor-lightcolor = "#${colors.base01}";
+        recolor-darkcolor = "#${colors.base05}";
+
+        index-fg = "#${colors.base05}";
+        index-bg = "#${colors.base01}";
+        index-active-fg = "#${colors.base05}";
+        index-active-bg = "#${colors.base02}";
+
+        render-loading-bg = "#${colors.base01}";
+        render-loading-fg = "#${colors.base05}";
+
+        highlight-color = "#${colors.base00}";
+        highlight-fg = "#${colors.base0F}";
+        highlight-active-color = "#${colors.base0F}";
       };
 
       mappings = {
