29003ac7c4a69566044f40880960891121f83988
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-05 22:13:17 -0400

    m/e/h/p/vscode.nix: use nix-community vscode extensions

--- modules/exclusive/home-manager/programs/vscode.nix
+++ modules/exclusive/home-manager/programs/vscode.nix
@@ -18,100 +18,100 @@ in {
       mutableExtensionsDir = true;
       enableExtensionUpdateCheck = true;
       enableUpdateCheck = true;
-      extensions = with extensions.vscode-marketplace; [
-        golang.go
-        vlanguage.vscode-vlang
-      ];
-
-      #      extensions = vscode-with-extensions.override {
-      #        vscode = vscodium;
-      #        vscodeExtensions = [
-      #          extensions.vscode-marketplace.golang.go
-      #          extensions.open-vsx-release.rust-lang.rust-analyzer
-      #        ];
-      #      };
-      # extensions = with pkgs.vscode-extensions;
-      #   [
-      #     arrterian.nix-env-selector
-      #     bbenoist.nix
-      #     catppuccin.catppuccin-vsc
-      #     christian-kohler.path-intellisense
-      #     dbaeumer.vscode-eslint
-      #     eamodio.gitlens
-      #     esbenp.prettier-vscode
-      #     formulahendry.code-runner
-      #     golang.go
-      #     ibm.output-colorizer
-      #     kamadorueda.alejandra
-      #     ms-azuretools.vscode-docker
-      #     ms-python.python
-      #     ms-python.vscode-pylance
-      #     ms-vscode-remote.remote-ssh
-      #     ms-vscode.cpptools
-
-      #     naumovs.color-highlight
-      #     ms-python.black-formatter
-      #     svelte.svelte-vscode
-      #     ms-vsliveshare.vsliveshare
-      #     oderwat.indent-rainbow
-      #     pkief.material-icon-theme
-      #     rust-lang.rust-analyzer
-      #     shardulm94.trailing-spaces
-      #     sumneko.lua
-      #     timonwong.shellcheck
-      #     usernamehw.errorlens
-      #     xaver.clang-format
-      #     yzhang.markdown-all-in-one
-      #     james-yu.latex-workshop
-      #     redhat.vscode-yaml
-      #     ms-azuretools.vscode-docker
-      #     irongeek.vscode-env
-      #     github.vscode-pull-request-github
-      #     github.codespaces
-      #     astro-build.astro-vscode
-      #     wakatime.vscode-wakatime
-      #   ]
-      #   ++ [
-      #     pkgs.vscode-extensions."2gua".rainbow-brackets
-      #   ]
-      #   ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      #     {
-      #       name = "copilot-nightly";
-      #       publisher = "github";
-      #       version = "1.67.7949";
-      #       sha256 = "sha256-ZtUqQeWjXmTz49DUeYkuqSTdVHRC8OfgWv8fuhlHDVc=";
-      #     }
-      #     #          {
-      #     #            name = "volar";
-      #     #            publisher = "vue";
-      #     #            version = "1.0.12";
-      #     #            sha256 = "sha256-D9E3KRUOlNVXH4oMv1W0+/mbqO8Se7+6E2F5P/KvCro=";
-      #     #          }
-      #     {
-      #       name = "vscode-typescript-vue-plugin";
-      #       publisher = "vue";
-      #       version = "1.0.12";
-      #       sha256 = "sha256-WiL+gc9+U861ubLlY/acR+ZcrFT7TdIDR0K1XNNidX8=";
-      #     }
-      #     {
-      #       name = "decay";
-      #       publisher = "decaycs";
-      #       version = "1.0.6";
-      #       sha256 = "sha256-Jtxj6LmHgF7UNaXtXxHkq881BbuPtIJGxR7kdhKr0Uo=";
-      #     }
-      #     {
-      #       name = "vscode-typescript-next";
-      #       publisher = "ms-vscode";
-      #       version = "5.0.202301100";
-      #       sha256 = "sha256-8d/L9F06ZaS9dTOXV6Q40ivI499nfZLQURcLdHXoTSM=";
-      #     }
-      #     {
-      #       name = "vscode-chromium-vector-icons";
-      #       publisher = "adolfdaniel";
-      #       version = "1.0.2";
-      #       sha256 = "sha256-Meo53e/3jUP6YDEXOA/40xghI77jj4iAQus3/S8RPZI=";
-      #     }
-      #   ];
+      extensions =
+        [
+          extensions.vscode-marketplace.golang.go
+          extensions.vscode-marketplace.vlanguage.vscode-vlang
+          extensions.open-vsx-release.rust-lang.rust-analyzer
+        ]
+        #      extensions = vscode-with-extensions.override {
+        #        vscode = vscodium;
+        #        vscodeExtensions = [
+        #          extensions.vscode-marketplace.golang.go
+        #          extensions.open-vsx-release.rust-lang.rust-analyzer
+        #        ];
+        #      };
+        # extensions = with pkgs.vscode-extensions;
+        #   [
+        #     arrterian.nix-env-selector
+        #     bbenoist.nix
+        #     catppuccin.catppuccin-vsc
+        #     christian-kohler.path-intellisense
+        #     dbaeumer.vscode-eslint
+        #     eamodio.gitlens
+        #     esbenp.prettier-vscode
+        #     formulahendry.code-runner
+        #     golang.go
+        #     ibm.output-colorizer
+        #     kamadorueda.alejandra
+        #     ms-azuretools.vscode-docker
+        #     ms-python.python
+        #     ms-python.vscode-pylance
+        #     ms-vscode-remote.remote-ssh
+        #     ms-vscode.cpptools
+        #     naumovs.color-highlight
+        #     ms-python.black-formatter
+        #     svelte.svelte-vscode
+        #     ms-vsliveshare.vsliveshare
+        #     oderwat.indent-rainbow
+        #     pkief.material-icon-theme
+        #     rust-lang.rust-analyzer
+        #     shardulm94.trailing-spaces
+        #     sumneko.lua
+        #     timonwong.shellcheck
+        #     usernamehw.errorlens
+        #     xaver.clang-format
+        #     yzhang.markdown-all-in-one
+        #     james-yu.latex-workshop
+        #     redhat.vscode-yaml
+        #     ms-azuretools.vscode-docker
+        #     irongeek.vscode-env
+        #     github.vscode-pull-request-github
+        #     github.codespaces
+        #     astro-build.astro-vscode
+        #     wakatime.vscode-wakatime
+        #   ]
+        #   ++ [
+        #     pkgs.vscode-extensions."2gua".rainbow-brackets
+        #   ]
+        ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "copilot-nightly";
+            publisher = "github";
+            version = "1.67.7949";
+            sha256 = "sha256-ZtUqQeWjXmTz49DUeYkuqSTdVHRC8OfgWv8fuhlHDVc=";
+          }
+          #          {
+          #            name = "volar";
+          #            publisher = "vue";
+          #            version = "1.0.12";
+          #            sha256 = "sha256-D9E3KRUOlNVXH4oMv1W0+/mbqO8Se7+6E2F5P/KvCro=";
+          #          }
+          {
+            name = "vscode-typescript-vue-plugin";
+            publisher = "vue";
+            version = "1.0.12";
+            sha256 = "sha256-WiL+gc9+U861ubLlY/acR+ZcrFT7TdIDR0K1XNNidX8=";
+          }
+          {
+            name = "decay";
+            publisher = "decaycs";
+            version = "1.0.6";
+            sha256 = "sha256-Jtxj6LmHgF7UNaXtXxHkq881BbuPtIJGxR7kdhKr0Uo=";
+          }
+          {
+            name = "vscode-typescript-next";
+            publisher = "ms-vscode";
+            version = "5.0.202301100";
+            sha256 = "sha256-8d/L9F06ZaS9dTOXV6Q40ivI499nfZLQURcLdHXoTSM=";
+          }
+          {
+            name = "vscode-chromium-vector-icons";
+            publisher = "adolfdaniel";
+            version = "1.0.2";
+            sha256 = "sha256-Meo53e/3jUP6YDEXOA/40xghI77jj4iAQus3/S8RPZI=";
+          }
+        ];
       userSettings = {
         "workbench.iconTheme" = "material-icon-theme";
         "workbench.colorTheme" = "Catppuccin Macchiato";
