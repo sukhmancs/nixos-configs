8acc686025db1a9e260be6e1f986be7026b56671
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-09 13:39:14 -0400

    m/e/h/p/vscode: temporary use catppuccino colorscheme until i have my own custom base16 based colorscheme for vscode

--- modules/exclusive/home-manager/desktops/hyprland/config/misc.nix
+++ modules/exclusive/home-manager/desktops/hyprland/config/misc.nix
@@ -20,7 +20,7 @@
       mouse_move_enables_dpms = true; # enable dpms on mouse/touchpad action
       key_press_enables_dpms = true; # enable dpms on keyboard action
       disable_autoreload = true; # autoreload is unnecessary on nixos, because the config is readonly anyway
-      background_color = "rgba(1D1011FF)";
+      # background_color = "rgba(1D1011FF)";
     };
   };
 }
