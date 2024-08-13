31d5c722bf8476a81cf1e4b1efc88cb22a9fea40
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-13 17:33:10 -0400

    m/s/security/apparmor.nix: setup more profiles for apparmor

--- modules/shared/security/ids.nix
+++ modules/shared/security/ids.nix
@@ -1,10 +1,12 @@
 #
 # IDS - Intrusion Detection System tools
+# Usage:
+#   `vulnix -S -D -j` for system scan, output with description, and json output
 #
 {pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
-      lynis
       vulnix
+      lynis
     ];
 }
