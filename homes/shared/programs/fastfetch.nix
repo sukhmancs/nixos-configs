7154f6639b00f0037f3bfcc6efb7cdc9096fca9e
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-13 04:44:01 -0400

    h/s/p/fastfetch.nix: make at as minimal as possible

--- homes/shared/programs/fastfetch.nix
+++ homes/shared/programs/fastfetch.nix
@@ -12,7 +12,9 @@
       "fastfetch/config.jsonc".text = builtins.toJSON {
         "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
         logo = {
-          source = "nixos"; #"nixos_small";
+          source = "nixos_small"; #"nixos_small";
+          # type = "iterm";
+          # source = "~/.config/fastfetch/rem.png";
           padding = {
             top = 2;
             left = 1;
@@ -20,7 +22,7 @@
           };
         };
         display = {
-          separator = "  ";
+          separator = "   ";
           size = {
             ndigits = 0;
             maxPrefix = "GB";
@@ -32,9 +34,14 @@
           };
           disableLinewrap = true;
           brightColor = true;
+          color = {
+            title = "default";
+            key = "default";
+            separator = "default";
+          };
         };
         modules = [
-          #"break"
+          "break"
           {
             type = "custom";
             format = "{#90}  {#31}  {#32}  {#33}  {#34}  {#35}  {#36}  {#37}";
@@ -45,34 +52,34 @@
             keyWidth = 10;
           }
           "break"
-          {
-            type = "cpu";
-            #          keyColor = "bright_cyan";
-            format = "{1}";
-            key = "  󰍛";
-          }
-          {
-            type = "gpu";
-            #          keyColor = "bright_cyan";
-            key = "  󰾲";
-            format = "{2}";
-          }
-          {
-            type = "memory";
-            #           keyColor = "bright_cyan";
-            key = "  󰑭";
-          }
-          {
-            type = "disk";
-            key = "  󱥎";
-            format = "{1} / {2} ({3})";
-            folders = "/";
-          }
-          {
-            type = "display";
-            #           keyColor = "bright_cyan";
-            key = "  󰍹";
-          }
+          # {
+          #   type = "cpu";
+          #   #          keyColor = "bright_cyan";
+          #   format = "{1}";
+          #   key = "  󰍛";
+          # }
+          # {
+          #   type = "gpu";
+          #   #          keyColor = "bright_cyan";
+          #   key = "  󰾲";
+          #   format = "{2}";
+          # }
+          # {
+          #   type = "memory";
+          #   #           keyColor = "bright_cyan";
+          #   key = "  󰑭";
+          # }
+          # {
+          #   type = "disk";
+          #   key = "  󱥎";
+          #   format = "{1} / {2} ({3})";
+          #   folders = "/";
+          # }
+          # {
+          #   type = "display";
+          #   #           keyColor = "bright_cyan";
+          #   key = "  󰍹";
+          # }
           {
             type = "os";
             key = "  ";
@@ -103,44 +110,44 @@
             type = "lm";
             key = "  󰧨";
           }
-          {
-            type = "terminalfont";
-            #           keyColor = "bright_magenta";
-            key = "  ";
-          }
-          {
-            type = "command";
-            #           keyColor = "bright_magenta";
-            key = "  󰉼";
-            text = "cat ~/.config/Kvantum/kvantum.kvconfig | grep theme | cut -d '=' -f 2";
-          }
-          {
-            type = "icons";
-            #           keyColor = "bright_magenta";
-            key = "  󰀻";
-          }
-          {
-            type = "cursor";
-            key = "  󰆿";
-            #           keyColor = "bright_magenta";
-          }
-          {
-            type = "command";
-            #           keyColor = "bright_magenta";
-            key = "  󰆧";
-            text = "(${lib.getExe' pkgs.nix "nix-store"} --query --requisites /run/current-system | wc -l | tr -d '\n') && echo ' (nix; /run/current-system)'";
-          }
-          {
-            type = "uptime";
-            #           keyColor = "bright_magenta";
-            key = "  󰅐";
-          }
-          {
-            type = "command";
-            #           keyColor = "bright_magenta";
-            key = "  󰃲";
-            text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
-          }
+          # {
+          #   type = "terminalfont";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  ";
+          # }
+          # {
+          #   type = "command";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  󰉼";
+          #   text = "cat ~/.config/Kvantum/kvantum.kvconfig | grep theme | cut -d '=' -f 2";
+          # }
+          # {
+          #   type = "icons";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  󰀻";
+          # }
+          # {
+          #   type = "cursor";
+          #   key = "  󰆿";
+          #   #           keyColor = "bright_magenta";
+          # }
+          # {
+          #   type = "command";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  󰆧";
+          #   text = "(${lib.getExe' pkgs.nix "nix-store"} --query --requisites /run/current-system | wc -l | tr -d '\n') && echo ' (nix; /run/current-system)'";
+          # }
+          # {
+          #   type = "uptime";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  󰅐";
+          # }
+          # {
+          #   type = "command";
+          #   #           keyColor = "bright_magenta";
+          #   key = "  󰃲";
+          #   text = "birth_install=$(stat -c %W /); current=$(date +%s); time_progression=$((current - birth_install)); days_difference=$((time_progression / 86400)); echo $days_difference days";
+          # }
           {
             type = "colors";
             paddingLeft = 1;
@@ -152,7 +159,7 @@
             type = "custom";
             format = "{#90}  {#31}  {#32}  {#33}  {#34}  {#35}  {#36}  {#37}";
           }
-          #"break"
+          "break"
         ];
       };
     };
