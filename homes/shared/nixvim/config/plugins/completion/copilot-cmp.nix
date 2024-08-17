b497c4a17dd468f34ae732595ddd39780c976f50
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-17 13:30:56 -0400

    setup nixvim - add plugins keymaps lua

--- homes/shared/nixvim/config/plugins/completion/copilot-cmp.nix
+++ homes/shared/nixvim/config/plugins/completion/copilot-cmp.nix
@@ -2,15 +2,16 @@
   programs.nixvim.plugins.copilot-cmp = {
     enable = true;
   };
-  programs.nixvim.plugins.copilot-lua = {
-    enable = true;
-    suggestion = {
-      enabled = false;
-    };
-    panel = {
-      enabled = false;
-    };
-  };
+  #Todo: throws an error nodejs-18_x missing
+  # programs.nixvim.plugins.copilot-lua = {
+  #   enable = true;
+  #   suggestion = {
+  #     enabled = false;
+  #   };
+  #   panel = {
+  #     enabled = false;
+  #   };
+  # };
 
   programs.nixvim.extraConfigLua = ''
     require("copilot").setup({
