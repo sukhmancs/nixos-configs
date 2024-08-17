b00c6a6dc5898629d5873651ff1dd1fe566b5933
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-17 13:57:42 -0400

    setup nixvim - add completions

--- homes/shared/nixvim/default.nix
+++ homes/shared/nixvim/default.nix
@@ -3,19 +3,13 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     ./config
-    # ./autocmd.nix
-    # ./colorscheme.nix
-    # ./extra.nix
-    # ./extraplug.nix
-    # ./highlight.nix
-    # ./keymap.nix
-    # ./plug.nix
-    # ./opts.nix
   ];
 
   config = {
     programs.nixvim = {
       enable = true;
+      viAlias = true;
+      vimAlias = true;
     };
   };
 }
