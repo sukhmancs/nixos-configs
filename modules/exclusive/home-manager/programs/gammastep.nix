f96cce88bf00423ab7ecfedce13a8d776d4ba409
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-18 01:32:35 -0400

    added some minor formatting changes

--- modules/exclusive/home-manager/programs/gammastep.nix
+++ modules/exclusive/home-manager/programs/gammastep.nix
@@ -1,17 +1,25 @@
 #
 # GammaStep - A tool to adjust the color temperature of your screen
 #
-{
-  lib,
-  osConfig,
-  config,
-  ...
-}: let
+{ lib
+, config
+, ...
+}:
+let
   inherit (lib) mkIf;
-in {
+in
+{
   config = mkIf config.services.gammastep.enable {
     services.gammastep = {
       provider = "geoclue2";
+      enableVerboseLogging = true;
+      temperature = {
+        day = 6000;
+        night = 4600;
+      };
+      settings = {
+        general.adjustment-method = "wayland";
+      };
     };
   };
 }
--- modules/roles/workstation/services/location.nix
+++ modules/roles/workstation/services/location.nix
@@ -1,9 +1,9 @@
-{config, ...}: {
+{
   location.provider = "geoclue2";
 
   services.geoclue2 = {
     # enable geoclue2 only if location.provider is geoclue2
-    enable = config.location.provider == "geoclue2";
+    enable = true;
 
     # TODO: make gammastep fall back to local if geoclue2 is disabled
     appConfig.gammastep = {
