9a2fa0bbb88272257d6518d5f84b11780507ab52
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-18 00:19:53 -0400

    revert back some changes to milkyway host

--- machines/milkyway/modules/device.nix
+++ machines/milkyway/modules/device.nix
@@ -6,7 +6,7 @@
       amd.pstate.enable = false;
       amd.zenpower.enable = false;
     };
-    gpu.type = null; #"hybrid-nv";
+    gpu.type = "hybrid-nv";
     monitors = [ "eDP-1" ];
     hasBluetooth = true;
     hasSound = true;
