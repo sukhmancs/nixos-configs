9469c7cd63f1d252a14e1f5727cace30b63a4c08
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-23 14:41:24 -0400

    update inputs

--- modules/exclusive/nixos/workstation/hyprland.nix
+++ modules/exclusive/nixos/workstation/hyprland.nix
@@ -1,23 +1,24 @@
-{
-  inputs',
-  config,
-  lib,
-  ...
-}: let
+{ inputs'
+, config
+, lib
+, ...
+}:
+let
   inherit (lib.modules) mkIf;
 
   hyprlandPkg = inputs'.hyprland.packages.hyprland;
   cfg = config.home-manager.users.xi;
-in {
+in
+{
   # disables Nixpkgs Hyprland module to avoid conflicts
-  disabledModules = ["programs/hyprland.nix"];
+  disabledModules = [ "programs/hyprland.nix" ];
 
   config = mkIf cfg.wayland.windowManager.hyprland.enable {
-    services.displayManager.sessionPackages = [hyprlandPkg];
+    services.displayManager.sessionPackages = [ hyprlandPkg ];
 
     xdg.portal = {
       enable = true;
-      configPackages = [hyprlandPkg];
+      configPackages = [ hyprlandPkg ];
       extraPortals = [
         (inputs'.xdg-portal-hyprland.packages.xdg-desktop-portal-hyprland.override {
           hyprland = hyprlandPkg;
