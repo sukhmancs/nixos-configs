a0948249fef48d76ca1a9fcdd517b458704c6a4b
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-28 03:59:01 -0400

    update todo list

--- modules/exclusive/nixos/workstation/hyprland.nix
+++ modules/exclusive/nixos/workstation/hyprland.nix
@@ -12,17 +12,20 @@ in {
   # disables Nixpkgs Hyprland module to avoid conflicts
   disabledModules = ["programs/hyprland.nix"];
 
-  config = mkIf ((config ? home-manager) && config.wayland.windowManager.hyprland.enable) {
-    services.displayManager.sessionPackages = [hyprlandPkg];
+  config =
+    mkIf
+    ((config ? home-manager)
+      && config.home-manager.users.xi.wayland.windowManager.hyprland.enable) {
+      services.displayManager.sessionPackages = [hyprlandPkg];
 
-    xdg.portal = {
-      enable = true;
-      configPackages = [hyprlandPkg];
-      extraPortals = [
-        (inputs'.xdg-portal-hyprland.packages.xdg-desktop-portal-hyprland.override {
-          hyprland = hyprlandPkg;
-        })
-      ];
+      xdg.portal = {
+        enable = true;
+        configPackages = [hyprlandPkg];
+        extraPortals = [
+          (inputs'.xdg-portal-hyprland.packages.xdg-desktop-portal-hyprland.override {
+            hyprland = hyprlandPkg;
+          })
+        ];
+      };
     };
-  };
 }
