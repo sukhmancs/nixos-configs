36d2469096650727c0546fd374196268426b4bf7
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-29 20:22:55 -0400

    m/e/h/p/gammastep: fix gammastep and set it up use manual lat lon

--- modules/exclusive/home-manager/programs/gammastep.nix
+++ modules/exclusive/home-manager/programs/gammastep.nix
@@ -11,14 +11,32 @@ in
 {
   config = mkIf config.services.gammastep.enable {
     services.gammastep = {
-      provider = "geoclue2";
+      tray = false;
       enableVerboseLogging = true;
       temperature = {
-        day = 6000;
-        night = 4600;
+        day = 5700;
+        night = 4000;
       };
       settings = {
-        general.adjustment-method = "wayland";
+        general = {
+          fade = "1";
+          brightness-day = "1.0";
+          brightness-night = "0.8";
+          location-provider = "manual";
+          adjustment-method = "wayland";
+
+          # Custom dawn/dusk intervals.
+          # Instead of using the solar elevation, the time intervals of dawn and dusk
+          # can be specified manually. The times must be specified as HH:MM in 24-hour
+          # format.
+          #dawn-time = "6:00-7:45";
+          #dusk-time = "18:35-20:15";
+        };
+        manual = {
+          # On, Canada
+          lat = "50.0"; # latitude
+          lon = "-85.0"; # longitude
+        };
       };
     };
   };
