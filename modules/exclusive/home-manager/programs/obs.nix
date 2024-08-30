6ac1e730c706ed78965da0973f61ac5abdba38ed
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-08-29 20:06:03 -0400

    m/e/h/p/obs.nix: add more plugins to obs studio

--- modules/exclusive/home-manager/programs/obs.nix
+++ modules/exclusive/home-manager/programs/obs.nix
@@ -1,23 +1,40 @@
-{
-  lib,
-  pkgs,
-  osConfig,
-  config,
-  ...
-}: let
+{ config, pkgs, lib, ... }:
+let
   inherit (lib) mkIf;
-  inherit (osConfig) modules meta;
-in {
+in
+{
   config = mkIf config.programs.obs-studio.enable {
-    programs.obs-studio = {
-      plugins = with pkgs.obs-studio-plugins;
-        [
-          obs-gstreamer
-          obs-pipewire-audio-capture
-          obs-vkcapture
-        ]
-        ++ optional meta.isWayland
-        pkgs.obs-studio-plugins.wlrobs;
+    programs = {
+      # live streaming
+      obs-studio = {
+        plugins = with pkgs.obs-studio-plugins;
+          [
+            # screen capture
+            wlrobs
+            # obs-ndi
+            obs-vaapi
+            obs-nvfbc
+            obs-teleport
+            # obs-hyperion
+            droidcam-obs
+            obs-vkcapture
+            obs-gstreamer
+            obs-3d-effect
+            input-overlay
+            obs-multi-rtmp
+            obs-source-clone
+            obs-shaderfilter
+            obs-source-record
+            obs-livesplit-one
+            looking-glass-obs
+            obs-vintage-filter
+            obs-command-source
+            obs-move-transition
+            obs-backgroundremoval
+            advanced-scene-switcher
+            obs-pipewire-audio-capture
+          ];
+      };
     };
   };
 }
