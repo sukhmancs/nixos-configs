728a0e326b23a803ff2152ae6f83324afcec0777
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-09-14 10:45:04 -0400

    update readme

--- machines/milkyway/modules/theme.nix
+++ machines/milkyway/modules/theme.nix
@@ -1,10 +1,10 @@
 #
-# Global theme
+# Global base16 themes
 #
 # My Ratings based on usability and aesthetics:
 # - 10 - catppuccino-mocha, alph, gruvbox-light-hard, catppuccino-frappe, emil
 # - 9.7 - gruvbox-dark-hard, ember
-# - 8.5 - ashes
+# - 8.5 - ashes, fairy-floss
 # - 6 - atelier-cave
 #
 # Note:
@@ -16,15 +16,6 @@
 #
 {
   modules.themes.colorscheme = {
-    # name = "catppuccin-mocha"; # Dark color theme (10/10)
-    # name = "alph"; # White color theme (10/10)
-    # name = "ashes"; # Dark color theme (8.5/10)
-    # name = "atelier-cave"; # Dark color theme (6/10)
-    # name = "catppuccin-frappe"; # Dark color theme (9/10)
-    # name = "gruvbox-dark-hard"; # Dark color theme (9.7/10)
-    # name = "gruvbox-light-hard"; # Light color theme (10/10)
-    # name = "ember";
-    # name = "emil";
-    name = "fairy-floss";
+    name = "catppuccin-mocha";
   };
 }
