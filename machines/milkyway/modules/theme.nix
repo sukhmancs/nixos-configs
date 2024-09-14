88e980356ae973925b5f3d29fc063787107f55f7
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-09-13 20:56:38 -0400

    testing with different themes

--- machines/milkyway/modules/theme.nix
+++ machines/milkyway/modules/theme.nix
@@ -1,8 +1,17 @@
 #
 # Global theme
 #
+# Note:
+# - Although I have implemented some serialization for the theme names, but I would
+# recommend to use the same name for the modules.themes.colorscheme.name as the folder name
+# in the /themes folder
+# - Changing the theme name is a big step, as it will build the theme from scratch for
+# a lot of applications, so be patient
+#
 {
   modules.themes.colorscheme = {
-    name = "catppuccin-mocha";
+    # name = "catppuccin-mocha";
+    # name = "alph"; # just make sure theme name is same as the folder name in /themes folder
+    name = "ashes";
   };
 }
