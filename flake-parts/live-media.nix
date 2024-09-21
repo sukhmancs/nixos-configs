e1a2a778eff617bafd3f0cbd12b93dce0683a325
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-09-20 23:51:58 -0400

    get rid of annoying lldap and authelia

--- flake-parts/live-media.nix
+++ flake-parts/live-media.nix
@@ -7,5 +7,6 @@
   flake.images = {
     # Installation iso
     messier = self.nixosConfigurations."messier".config.system.build.isoImage;
+    nebula = self.nixosConfigurations."nebula".config.system.build.isoImage;
   };
 }
