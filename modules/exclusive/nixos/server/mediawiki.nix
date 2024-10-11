82f055251b5e56b61f0fff313851f06c0ec0938b
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-10-11 18:06:47 -0400

    remove firefox

--- modules/exclusive/nixos/server/mediawiki.nix
+++ modules/exclusive/nixos/server/mediawiki.nix
@@ -17,35 +17,37 @@
   inherit (cfg.mediawiki.settings) port host;
 in {
   config = mkIf config.services.mediawiki.enable {
-    services.mediawiki = {
+    services = {
+      mediawiki = {
         webserver = "nginx";
         httpd.virtualHost.listen = [
-            {
-                ip = host;
-                port = port;
-                ssl = false;
-            }
+          {
+            ip = host;
+            port = port;
+            ssl = false;
+          }
         ];
         # Administrator account username is admin.
         # Set initial password to "cardbotnine" for the account admin.
         passwordFile = pkgs.writeText "password" "cardbotnine";
         extraConfig = ''
-            # Disable anonymous editing
-            $wgGroupPermissions['*']['edit'] = false;
+          # Disable anonymous editing
+          $wgGroupPermissions['*']['edit'] = false;
         '';
 
         extensions = {
-            # some extensions are included and can enabled by passing null
-            VisualEditor = null;
+          # some extensions are included and can enabled by passing null
+          VisualEditor = null;
 
-            # https://www.mediawiki.org/wiki/Extension:TemplateStyles
-            TemplateStyles = pkgs.fetchzip {
+          # https://www.mediawiki.org/wiki/Extension:TemplateStyles
+          TemplateStyles = pkgs.fetchzip {
             url = "https://extdist.wmflabs.org/dist/extensions/TemplateStyles-REL1_40-c639c7a.tar.gz";
             hash = "sha256-YBL0Cs4hDSNnoutNJSJBdLsv9zFWVkzo7m5osph8QiY=";
-            };
+          };
         };
+      };
 
-        nginx.virtualHosts.${domain} =
+      nginx.virtualHosts.${domain} =
         {
           locations."/" = {
             proxyPass = "http://${host}:${toString port}";
