cea0fa759d690451652288ef7caf7ff97ece399e
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-10-17 20:15:47 -0400

    setup joplin

--- modules/exclusive/home-manager/programs/joplin.nix
+++ modules/exclusive/home-manager/programs/joplin.nix
@@ -1,17 +1,19 @@
-{ config
-, lib
-, ...
-}:
-let
-  inherit (lib) mkIf;
-in
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) mkIf;
+in {
   config = mkIf config.programs.joplin-desktop.enable {
     programs.joplin-desktop = {
+      package = pkgs.joplin-desktop;
       general.editor = null;
       extraConfig = {
         "markdown.plugin.mark" = true;
         newNoteFocus = "title";
+        "themeAutoDetect" = true;
       };
       sync = {
         interval = "10m";
