7573ea78cc36512cc501ed393911cfaade98c49d
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-10-20 14:57:20 -0400

    added some tweaks

--- flake-parts/templates/python/flake.nix
+++ flake-parts/templates/python/flake.nix
@@ -55,6 +55,8 @@
             export PYLINTRC="$XDG_CONFIG_HOME/pylint/pylintrc"
             export WORKON_HOME="$XDG_DATA_HOME/virtualenvs"
 
+            # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+            # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
             export PIP_PREFIX=$(pwd)/_build/pip_packages
             export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
             export PATH="$PIP_PREFIX/bin:$PATH"
