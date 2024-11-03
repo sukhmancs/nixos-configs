06760644991648c33353e733ec047eb0b9a80128
racoot <63659772+sukhmancs@users.noreply.github.com>
2024-11-03 16:23:05 -0500

    added minor tweaks

--- modules/exclusive/home-manager/programs/swayidle.nix
+++ modules/exclusive/home-manager/programs/swayidle.nix
@@ -23,6 +23,14 @@
     then 4 * 60
     else 10 * 60; # 4 minutes for laptops, 10 minutes for desktops
 
+  suspendScript = pkgs.writeShellScript "suspend-script" ''
+    ${pkgs.pipewire}/bin/pw-cli i all | ${pkgs.ripgrep}/bin/rg running
+    # only suspend if audio isn't running
+    if [ $? == 1 ]; then
+      ${systemctl} suspend
+    fi
+  '';
+
   # Makes two timeouts: one for when the screen is not locked (lockTime+timeout) and one for when it is.
   afterLockTimeout = {
     timeout,
@@ -51,39 +59,60 @@ in {
   config = mkIf config.services.swayidle.enable {
     services.swayidle = {
       systemdTarget = "graphical-session.target";
-      timeouts =
-        # Rather than relying on some weird scripts to dim the screen,
-        # We can just use brightnessctl to do it for us.
-        # Dim screen before locking
-        [
-          {
-            timeout = lockTime - 10; # Dim the screen 10 seconds before locking
-            command = "${isNoAudioRunning} && ${brightnessctl} set 10%";
-            resumeCommand = "${brightnessctl} set 100%"; # Restore brightness on resume
-          }
-        ]
-        ++
-        # Lock screen
-        [
-          {
-            timeout = lockTime;
-            command = "${isNoAudioRunning} && ${brightnessctl} set 100% && ${swaylock} --image ${modules.themes.wallpaper}";
-          }
-        ]
-        ++
-        # Turn off displays (hyprland)
-        (lib.optionals config.wayland.windowManager.hyprland.enable (afterLockTimeout {
-          timeout = 5;
-          command = "${isNoAudioRunning} && ${hyprctl} dispatch dpms off";
-          resumeCommand = "${hyprctl} dispatch dpms on";
-        }))
-        ++
-        # Turn off displays (sway)
-        (lib.optionals config.wayland.windowManager.sway.enable (afterLockTimeout {
-          timeout = 40;
-          command = "${isNoAudioRunning} && ${swaymsg} 'output * dpms off'";
-          resumeCommand = "${swaymsg} 'output * dpms on'";
-        }));
+      events = [
+        {
+          event = "before-sleep";
+          command = "${pkgs.systemd}/bin/loginctl lock-session";
+        }
+        {
+          event = "lock";
+          command = "${swaylock} --image ${modules.themes.wallpaper}";
+        }
+      ];
+      timeouts = [
+        {
+          timeout = 900;
+          command = suspendScript.outPath;
+        }
+        {
+          timeout = 1200;
+          command = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms off";
+          resumeCommand = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch dpms on";
+        }
+      ];
+      # timeouts =
+      #   # Rather than relying on some weird scripts to dim the screen,
+      #   # We can just use brightnessctl to do it for us.
+      #   # Dim screen before locking
+      #   [
+      #     {
+      #       timeout = lockTime - 10; # Dim the screen 10 seconds before locking
+      #       command = "${isNoAudioRunning} && ${brightnessctl} set 10%";
+      #       resumeCommand = "${brightnessctl} set 100%"; # Restore brightness on resume
+      #     }
+      #   ]
+      #   ++
+      #   # Lock screen
+      #   [
+      #     {
+      #       timeout = lockTime;
+      #       command = "${isNoAudioRunning} && ${brightnessctl} set 100% && ${swaylock} --image ${modules.themes.wallpaper}";
+      #     }
+      #   ]
+      #   ++
+      #   # Turn off displays (hyprland)
+      #   (lib.optionals config.wayland.windowManager.hyprland.enable (afterLockTimeout {
+      #     timeout = 5;
+      #     command = "${isNoAudioRunning} && ${hyprctl} dispatch dpms off";
+      #     resumeCommand = "${hyprctl} dispatch dpms on";
+      #   }))
+      #   ++
+      #   # Turn off displays (sway)
+      #   (lib.optionals config.wayland.windowManager.sway.enable (afterLockTimeout {
+      #     timeout = 40;
+      #     command = "${isNoAudioRunning} && ${swaymsg} 'output * dpms off'";
+      #     resumeCommand = "${swaymsg} 'output * dpms on'";
+      #   }));
     };
   };
 }
